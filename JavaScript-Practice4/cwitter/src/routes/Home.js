253eb7b976c358dda9935717af09175285c16a7a
csh94 <speed9116@naver.com>
2021-01-23 19:23:58 +0900

    Part. 3 TWEETING_Cweeting!

--- JavaScript-Practice4/cwitter/src/routes/Home.js
+++ JavaScript-Practice4/cwitter/src/routes/Home.js
@@ -1,9 +1,15 @@
+import { dbService } from "fbase";
 import React, { useState } from "react";
 
 const Home = () => {
     const [cweet, setCweet] = useState("");
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
       event.preventDefault();  
+      await dbService.collection("cweets").add({
+          cweet,
+          createdAt: Date.now(),
+      });
+      setCweet("");
     };
     const onChange = (event) => {
         const {target:{value}} = event;
@@ -12,7 +18,7 @@ const Home = () => {
     //event로부터 라는 의미지. 즉, event 안에 있는 target안에 있는 value를 달라.
     return (
         <div>    
-            <form>
+            <form onSubmit={onSubmit}>
                 <input value={cweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
                 <input type="submit" value="Cweet" />
             </form>
