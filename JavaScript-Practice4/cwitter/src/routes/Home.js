f3b31680955e002e9407dfdd0d8a41130d0621f0
csh94 <speed9116@naver.com>
2021-01-23 18:56:36 +0900

    Part. 3 TWEETING_From and Database Setup

--- JavaScript-Practice4/cwitter/src/routes/Home.js
+++ JavaScript-Practice4/cwitter/src/routes/Home.js
@@ -1,4 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [cweet, setCweet] = useState("");
+    const onSubmit = (event) => {
+      event.preventDefault();  
+    };
+    const onChange = (event) => {
+        const {target:{value}} = event;
+        setCweet(value);
+    };
+    //event로부터 라는 의미지. 즉, event 안에 있는 target안에 있는 value를 달라.
+    return (
+        <div>    
+            <form>
+                <input value={cweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+                <input type="submit" value="Cweet" />
+            </form>
+        </div>
+    );
+};
 export default Home;
