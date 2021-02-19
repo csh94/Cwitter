4565f04c6eeaa68e3af72398c813ecd51d504165
csh94 <speed9116@naver.com>
2021-02-19 21:24:46 +0900

    Part.5 EDIT PROFILE_Update Profile

--- JavaScript-Practice4/cwitter/src/routes/Profile.js
+++ JavaScript-Practice4/cwitter/src/routes/Profile.js
@@ -1,29 +1,54 @@
 import { authService, dbService } from "fbase";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 
 export default ({ userObj }) => {
     const history = useHistory();
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
     const onLogOutClick = () => {
         authService.signOut();
         history.push("/");
     };
 
     //어떻게 DB 데이터를 필터링하는지를 보여주고싶었어.
-    const getMyCweets = async() => {
+    /* const getMyCweets = async() => {
         const cweets = await dbService
         .collection("cweets")
         .where("creatorId", "==", userObj.uid)
         .orderBy("createrAt")
         .get();
         console.log(cweets.docs.map((doc) => doc.data()));
+        console.log(userObj.displayName);
     };
     useEffect(() => {
         getMyCweets();
-    }, [])
+    }, []); */
+
+    const onChange = (event) => {
+        const {
+            target: {value},
+        } = event;
+        setNewDisplayName(value);
+    };
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        if(userObj.displayName !== newDisplayName) {
+            await userObj.updateProfile({
+                displayName: newDisplayName,
+            });
+        }
+    };
 
     return (
         <>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange}
+                type="text"
+                placeholder="Display name"
+                value={newDisplayName} 
+                />
+                <input type="submit" value="Update Profile" />
+            </form>
             <button onClick={onLogOutClick}>Log Out</button>
         </>
     );
